2da935664c0aa55f64639c79292874601f332cdc
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2021-09-19 23:59:27 +0530

    Update test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+
+from dataset import *
+import config
+
+epochs=config.max_epoch
+train_batch_size=config.train_batch_size
+data_path=config.data_path
+
+
+dataset = Dataset(data_path=data_path)
+data_loader = Data_loader(dataset,num_workers=num_workers,batch_size=train_batch_size,shuffle=shuffle)
+
+test(epochs=epochs, model=model, train_loader=train_loader, val_loader=val_loader, criterion=criterion, optimizer=optimizer, scheduler=scheduler, checkpoint_path, best_model_path)
