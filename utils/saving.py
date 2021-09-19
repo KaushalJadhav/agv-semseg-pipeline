06ebbf039682c703834a2e27f1e5f7251ff4f6a3
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2021-09-19 23:18:52 +0530

    added make_dict function

--- utils/saving.py
+++ utils/saving.py
@@ -33,3 +33,7 @@ def load_ckp(checkpoint_fpath, model, optimizer):
     valid_loss_min = checkpoint['val_loss_min']
     # return model, optimizer, epoch value, min validation loss 
     return model, checkpoint['epoch'], valid_loss_min, optimizer
+
+def make_dict(model,optim,epoch,loss,accuracy,iou,wandb_run,artifact_name):
+    state = {'epoch':epoch,'model':model.state_dict(),'optimizer': optim.state_dict(),'loss':loss,'accuracy':accuracy,'iou':iou}
+    return state
