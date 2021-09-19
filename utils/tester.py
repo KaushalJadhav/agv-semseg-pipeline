6db5064f6c675e614de2147ecde8c05886c1541b
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2021-09-20 00:13:30 +0530

    updated tester.py

--- utils/tester.py
+++ utils/tester.py
@@ -4,54 +4,51 @@ import metrics
 import numpy as np 
 import torch.nn.functional as F
 
+def final_metrics(self,config.num_classes):
+    ENet.eval()
     
-class Tester():
-    def __init__(self):
-        pass
-    def final_metrics(self,config.num_classes):
-     ENet.eval()
+    train_accuracy = np.zeros((config.num_classes,), dtype=float)
+    train_iou = np.zeros((config.num_classes,), dtype=float)
     
-     train_accuracy = np.zeros((config.num_classes,), dtype=float)
-     train_iou = np.zeros((config.num_classes,), dtype=float)
-    
-     val_accuracy = np.zeros((config.num_classes,), dtype=float)
-     val_iou = np.zeros((config.num_classes,), dtype=float)
+    val_accuracy = np.zeros((config.num_classes,), dtype=float)
+    val_iou = np.zeros((config.num_classes,), dtype=float)
 
-     val_results = []
+    val_results = []
       
-     for batch in CityScapesDataLoader.train_DataLoader:
+    for batch in CityScapesDataLoader.train_DataLoader:
         
-         inputs = batch[0].float().to(device)
-         labels = batch[1].float().to(device).long()
+        inputs = batch[0].float().to(device)
+        labels = batch[1].float().to(device).long()
         
-         outputs = ENet(inputs)
+        outputs = ENet(inputs)
 
-         iou, accu = metrics(outputs, labels, config.num_classes, True)
+        iou, accu = metrics(outputs, labels, config.num_classes, True)
         
-         train_accuracy += accu
-         train_iou += iou
+        train_accuracy += accu
+        train_iou += iou
     
-     for batch in CityScapesDataLoader.val_DataLoader:
+    for batch in CityScapesDataLoader.val_DataLoader:
         
-         inputs = batch[0].float().to(device)
-         labels = batch[1].float().to(device).long()
+        inputs = batch[0].float().to(device)
+        labels = batch[1].float().to(device).long()
 
-         outputs = ENet(inputs)
+        outputs = ENet(inputs)
         
-         np_outputs, iou, accu = metrics(outputs, labels, config.num_classes)
+        np_outputs, iou, accu = metrics(outputs, labels, config.num_classes)
         
-         val_accuracy += accu
-         val_iou += iou
-         val_results.append(np_outputs)
+        val_accuracy += accu
+        val_iou += iou
+        val_results.append(np_outputs)
         
-     train_accuracy /= len(CityScapesDataLoader.train_DataLoader)
-     val_accuracy /= len(CityScapesDataLoader.val_DataLoader)
+    train_accuracy /= len(CityScapesDataLoader.train_DataLoader)
+    val_accuracy /= len(CityScapesDataLoader.val_DataLoader)
     
-     train_iou /= len(CityScapesDataLoader.train_DataLoader)
-     val_iou /= len(CityScapesDataLoader.val_DataLoader)
+    train_iou /= len(CityScapesDataLoader.train_DataLoader)
+    val_iou /= len(CityScapesDataLoader.val_DataLoader)
 
-     val_results = np.array(val_results)
+    val_results = np.array(val_results)
 
-     return train_accuracy, val_accuracy, train_iou, val_iou, val_results
+    return train_accuracy, val_accuracy, train_iou, val_iou, val_results
+    
 
 
