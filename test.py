83b5e8d5631cb0d1d80f292c63429bd844a25e1e
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2021-09-20 00:23:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,47 @@
+  
+from utils.losses import CrossEntropyLoss
+from utils.device import default_device
 
-from dataset import *
+from models.enet import ENet
+from dataloader.cityscapes import CityScapesDataLoader
 
-#This will call trainer.py which is just the basic function. All the arguements like ptimizer, scheduler etc will be provided from here to trainer.
-# we need to make this train.py in class so that we can it from run.py
+import torch 
+import numpy as np 
+import torch
+
+#This will call trainer.py which is just the basic function. All the arguements like optimizer, scheduler etc will be provided from here to trainer.
+# we need to make this test.py in class so that we can it from run.py
+#copied most stuff from train.py mradul made and made some minor changes to it
+
 class test:
-  def __init__(self, model, config):
-    self.epochs=config.max_epoch
-    self.train_batch_size=config.train_batch_size
-    self.data_path=config.data_path
+    def __init__(self, config):
+        self.config = config
+        self.model = ENet(self.config)
+        self.dataloader = CityScapesDataLoader(self.config)
+        self.loss = CrossEntropyLoss(self.config)
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                          lr=self.config.learning_rate,
+                                          weight_decay=self.config.weight_decay)
+
+        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer,
+                                                                gamma=self.config.gamma)
+
+        self.valid_dataloader = self.dataloader.valid_dataloader
+        self.train_dataloader = self.dataloader.valid_datalaoder
+
+        self.current_epoch = 1
+
+        self.device = default_device()
+
+        self.model = self.model.to(self.device)
+        self.loss = self.loss.to(self.device)
+
+    def train(self):
+      test(epochs=self.epochs, model=model, train_loader=train_loader, val_loader=val_loader, criterion=self.criterion, optimizer=self.optimizer, scheduler=self.scheduler, checkpoint_path, best_model_path)
+
+    def validate(self):
 
 
-    self.dataset = Dataset(data_path=data_path)
-    self.data_loader = Data_loader(dataset,num_workers=num_workers,batch_size=train_batch_size,shuffle=shuffle)
 
-  test(epochs=self.epochs, model=model, train_loader=train_loader, val_loader=val_loader, criterion=self.criterion, optimizer=self.optimizer, scheduler=self.scheduler, checkpoint_path, best_model_path)
-
+    def finalize(self): 
