fc09ecf509f91eeae0f74b627c1d16e20d57e0f2
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2021-09-19 23:25:29 +0530

    Update tester.py

--- utils/tester.py
+++ utils/tester.py
@@ -2,49 +2,58 @@ from models.enet import ENet
 from dataloader.cityscapes import CityScapesDataLoader
 
 import torch.nn.functional as F
-def final_metrics(train_DataLoader, val_DataLoader, model, num_classes):
-    model.eval()
-    
-    train_accuracy = np.zeros((num_classes,), dtype=float)
-    train_iou = np.zeros((num_classes,), dtype=float)
-    
-    val_accuracy = np.zeros((num_classes,), dtype=float)
-    val_iou = np.zeros((num_classes,), dtype=float)
 
-    val_results = []
-      
-    for batch in train_DataLoader:
+class Test:
+    def __init__(self, model, data_loader, criterion, metric, device):
+        self.model = model
+        self.data_loader = data_loader
+        self.criterion = criterion
+        self.metric = metric
+        self.device = device
         
-        inputs = batch[0].float().to(device)
-        labels = batch[1].float().to(device).long()
-        
-        outputs = model(inputs)
+    def final_metrics(train_DataLoader, val_DataLoader, model, num_classes):
+        model.eval()
 
-        iou, accu = metrics(outputs, labels, num_classes, True)
-        
-        train_accuracy += accu
-        train_iou += iou
-    
-    for batch in val_DataLoader:
-        
-        inputs = batch[0].float().to(device)
-        labels = batch[1].float().to(device).long()
+        train_accuracy = np.zeros((num_classes,), dtype=float)
+        train_iou = np.zeros((num_classes,), dtype=float)
 
-        outputs = model(inputs)
-        
-        np_outputs, iou, accu = metrics(outputs, labels, num_classes)
-        
-        val_accuracy += accu
-        val_iou += iou
-        val_results.append(np_outputs)
-        
-    train_accuracy /= len(train_DataLoader)
-    val_accuracy /= len(val_DataLoader)
-    
-    train_iou /= len(train_DataLoader)
-    val_iou /= len(val_DataLoader)
+        val_accuracy = np.zeros((num_classes,), dtype=float)
+        val_iou = np.zeros((num_classes,), dtype=float)
 
-    val_results = np.array(val_results)
+        val_results = []
 
-    return train_accuracy, val_accuracy, train_iou, val_iou, val_results
-    
+        for batch in train_DataLoader:
+
+            inputs = batch[0].float().to(device)
+            labels = batch[1].float().to(device).long()
+
+            outputs = self.model(inputs)
+
+            iou, accu = metrics(outputs, labels, num_classes, True)
+
+            train_accuracy += accu
+            train_iou += iou
+
+        for batch in val_DataLoader:
+
+            inputs = batch[0].float().to(device)
+            labels = batch[1].float().to(device).long()
+
+            outputs = self.model(inputs)
+
+            np_outputs, iou, accu = metrics(outputs, labels, num_classes)
+
+            val_accuracy += accu
+            val_iou += iou
+            val_results.append(np_outputs)
+
+        train_accuracy /= len(train_DataLoader)
+        val_accuracy /= len(val_DataLoader)
+
+        train_iou /= len(train_DataLoader)
+        val_iou /= len(val_DataLoader)
+
+        val_results = np.array(val_results)
+
+        return train_accuracy, val_accuracy, train_iou, val_iou, val_results
+
