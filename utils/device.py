438ed895d8d85d5a8a8104f568eccb2d14a3c39e
Mradul Agrawal <mradulag20@gmail.com>
2021-09-19 22:49:50 +0530

    Baseline

--- utils/device.py
+++ utils/device.py
@@ -1,7 +1,8 @@
+import torch
+
 def default_device():
     """Pick GPU if available, else CPU"""
     if torch.cuda.is_available():
-        return 'cudaa'
+        return 'cuda'
     else:
         return 'cpu'
-device = default_device()
