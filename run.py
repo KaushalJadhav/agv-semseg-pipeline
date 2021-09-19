4120479e5b9f56bfc2da8b6b470ba121e3f8f3c4
Manideep Sridhara <73156028+manideep1108@users.noreply.github.com>
2021-09-20 01:50:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import train, test
 from utils.config import process_config
+from utils.preprocessing import encode_data
+from dataloader.cityscapes import CityScapes, load_data, get_data_array, get_data
+from dataloader.cityscapesCutmix import CityScapesCutmix, load_data, get_data_array, get_data
 
 def main():
     arg_parser = argparse.ArgumentParser(description="")
@@ -14,6 +17,11 @@ def main():
     # Parse the config json file
     config = process_config(args.config)
 
+
+train_X, train_y, val_X, val_y, test_X = get_data_array(config["train_X_path"], config["train_y_path"], config["val_X_path"], config["val_y_path"], config["test_X_path"])
+train_y_encoded, val_y_encoded = encode_data(train_y, val_y)
+train_data, val_data = get_data(train_X, train_y_encoded, val_X, val_y_encoded)
+train_loader, val_loader = load_data(train_data, val_data)
     
 
 
