1b832dbde5b6dc9e26e19ebe9967ca177813ed1b
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2021-09-20 14:23:05 +0530

    updated train.py

--- train.py
+++ train.py
@@ -1,20 +1,15 @@
-from utils.losses import CrossEntropyLoss
-from utils.device import default_device
+from dataset import *
+import torch.optim as optim
+import torch.nn as nn          #We wll make this modular by adding these either to config
 
 from models.enet import ENet
 import torch 
 import numpy as np 
 
 
-# epochs=config.max_epoch
-# train_batch_size=config.train_batch_size
+#wandb 
 
-
-# trainer(epochs=epochs, model=model, train_loader=train_loader, val_loader=val_loader, criterion=criterion, optimizer=optimizer, scheduler=scheduler, checkpoint_path, best_model_path)
-
-
-
-class ENetAgent():
+class ENet():
     def __init__(self, config):
         self.config = config
         self.model = ENet(self.config)
@@ -34,21 +29,18 @@ class ENetAgent():
         self.current_epoch = 1
 
         self.device = default_device()
-
         self.model = self.model.to(self.device)
         self.loss = self.loss.to(self.device)
 
     def train(self):
-        fit(epochs, model, train_loader, val_loader, criterion, optimizer, scheduler, num_classes, checkpoint_path, best_model_path)
+        train_dataset = Dataset(data_path=train_data_path)
+        val_dataset   = Dataset(data_path=val_data_path)
 
+        train_loader = Data_loasder(dataset,num_workers=num_workers,batch_size=train_batch_size,shuffle=shuffle)
+        val_loader   = Data_loasder(dataset,num_workers=num_workers,batch_size=train_batch_size,shuffle=shuffle)
 
-    def validate(self):
-        pass
-
-
-
-    def finalize(self):
-        pass
+        trainer(epochs=self.epochs, model=self.model, train_loader=self.train_dataloader, val_loader=self.val_dataloader, criterion=self.loss, optimizer=self.optimizer, scheduler=self.scheduler, checkpoint_path, best_model_path)
+        #Here still we have to fix the path to where we log the data
 
 
     
