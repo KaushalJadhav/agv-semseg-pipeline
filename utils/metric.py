ec85030205520062f2daa412058d89db0ae4f811
Mradul Agrawal <mradulag20@gmail.com>
2021-09-20 18:43:15 +0530

    Corrected tester.py and metric.py

--- utils/metric.py
+++ utils/metric.py
@@ -1,18 +1,21 @@
-SMOOTH = 1e-6
+import torch
+import numpy as np
 
-def metrics(outputs, labels, num_classes, train = False):
+# These are utility functions, we need not to initialize any dataloader or model here (ENet or Cityscapes)
+# Just pass those in these functions wherever these are called (test.py or train.py) ~mradul
 
-    accu = np.zeros((num_classes,), dtype=float)
-    iou = np.zeros((num_classes,), dtype=float)
+def iou_accu(config, outputs, labels):
+    accu = np.zeros((config.num_classes,), dtype=float)
+    iou = np.zeros((config.num_classes,), dtype=float)
 
     output_cvt = torch.argmax(outputs, dim=1)
 
     np_outputs = output_cvt.cpu().detach().numpy()
     np_labels = labels.cpu().detach().numpy()
 
-    np_outputs[np_labels == 19] = 19
+    np_outputs[np_labels == config.ignore_class] = config.ignore_class
 
-    for x in range(num_classes):
+    for x in range(config.num_classes):
         output_mask = (np_outputs == x)
         label_mask = (np_labels == x)
     
@@ -20,10 +23,7 @@ def metrics(outputs, labels, num_classes, train = False):
         union = (output_mask | label_mask).sum((1, 2))
         total = label_mask.sum((1, 2))
 
-        iou[x] = ((intersection + SMOOTH) / (union + SMOOTH)).mean()
-        accu[x] = ((intersection + SMOOTH) / (total + SMOOTH)).mean()
+        iou[x] = ((intersection + config.SMOOTH) / (union + config.SMOOTH)).mean()
+        accu[x] = ((intersection + config.SMOOTH) / (total + config.SMOOTH)).mean()
     
-    if train:
-        return iou, accu
-    else: 
-        return np_outputs.squeeze(), iou, accu
+    return iou, accu
--- utils/tester.py
+++ utils/tester.py
@@ -1,54 +1,48 @@
-from ENets.enet import ENet
-from dataloader.cityscapes import CityScapesDataLoader
-import metrics
-import numpy as np 
-import torch.nn.functional as F
+from metric import iou_accu
+import numpy as np
 
-def final_metrics(config.num_classes):
-    ENet.eval()
+# These are utility functions, we need not to initialize any dataloader or model here (ENet or Cityscapes)
+# Just pass those in these functions wherever these are called (test.py or train.py) ~mradul
+
+def final_metrics(config, model, train_loader, valid_loader, device):
+    model.eval()
     
     train_accuracy = np.zeros((config.num_classes,), dtype=float)
     train_iou = np.zeros((config.num_classes,), dtype=float)
     
     val_accuracy = np.zeros((config.num_classes,), dtype=float)
     val_iou = np.zeros((config.num_classes,), dtype=float)
-
-    val_results = []
       
-    for batch in CityScapesDataLoader.train_DataLoader:
+    for batch in train_loader:
         
         inputs = batch[0].float().to(device)
         labels = batch[1].float().to(device).long()
         
-        outputs = ENet(inputs)
+        outputs = model(inputs)
 
-        iou, accu = metrics(outputs, labels, config.num_classes, True)
+        iou, accu = iou_accu(config, outputs, labels)
         
         train_accuracy += accu
         train_iou += iou
     
-    for batch in CityScapesDataLoader.val_DataLoader:
+    for batch in valid_loader:
         
         inputs = batch[0].float().to(device)
         labels = batch[1].float().to(device).long()
 
-        outputs = ENet(inputs)
+        outputs = model(inputs)
         
-        np_outputs, iou, accu = metrics(outputs, labels, config.num_classes)
+        np_outputs, iou, accu = iou_accu(config, outputs, labels)
         
         val_accuracy += accu
         val_iou += iou
-        val_results.append(np_outputs)
         
-    train_accuracy /= len(CityScapesDataLoader.train_DataLoader)
-    val_accuracy /= len(CityScapesDataLoader.val_DataLoader)
+    train_accuracy /= len(train_loader)
+    val_accuracy /= len(val_loader)
     
-    train_iou /= len(CityScapesDataLoader.train_DataLoader)
-    val_iou /= len(CityScapesDataLoader.val_DataLoader)
+    train_iou /= len(train_loader)
+    val_iou /= len(val_loader)
 
     val_results = np.array(val_results)
 
-    return train_accuracy, val_accuracy, train_iou, val_iou, val_results
-    
-
-
+    return train_accuracy, val_accuracy, train_iou, val_iou
