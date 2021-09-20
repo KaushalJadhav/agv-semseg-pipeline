34cf671b1788fa0c7b32b834a607e33872f4bce3
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2021-09-20 16:24:07 +0530

    updated train.py

--- train.py
+++ train.py
@@ -33,11 +33,6 @@ class Train():
         self.loss = self.loss.to(self.device)
 
     def forward(self):
-        train_dataset = Dataset(data_path=train_data_path)
-        val_dataset   = Dataset(data_path=val_data_path)
-
-        train_loader = Data_loasder(dataset,num_workers=num_workers,batch_size=train_batch_size,shuffle=shuffle)
-        val_loader   = Data_loasder(dataset,num_workers=num_workers,batch_size=train_batch_size,shuffle=shuffle)
 
         trainer(epochs=self.epochs, model=self.model, train_loader=self.train_dataloader, val_loader=self.val_dataloader, criterion=self.loss, optimizer=self.optimizer, scheduler=self.scheduler, checkpoint_path, best_model_path)
         #Here still we have to fix the path to where we log the data
